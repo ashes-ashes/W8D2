fb24e0383b3b72e694a5159ebe4d52e61cf6c3ca
Ash Todd <ashztodd@gmail.com>
2019-08-13 16:58:53 -0700

    Partway through V

--- skeleton/lib/router.rb
+++ skeleton/lib/router.rb
@@ -1,16 +1,25 @@
+
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (@pattern =~ req.path) && (@http_method.to_s == req.request_method.downcase)
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    # @params = req.path.match(@pattern)
+    # @controller_class.new.send(@action_name)
   end
 end
 
@@ -18,15 +27,18 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    
   end
 
   # make each of these methods that
