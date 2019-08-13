5720d83aeb70e57ebf74bbaa0bd8732d3ba7e209
Ash Todd <ashztodd@gmail.com>
2019-08-13 16:11:47 -0700

    Finished Phase IV

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -24,6 +24,7 @@ class ControllerBase
       @res['Location'] = url
       @res.status = 302
       @already_built_response = true
+      session.store_session(res)
     end
   end
 
@@ -37,6 +38,7 @@ class ControllerBase
       @res['Content-Type'] = content_type
       @res.write(content)
       @already_built_response = true
+      session.store_session(res)
     end
   end
 
@@ -51,6 +53,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
